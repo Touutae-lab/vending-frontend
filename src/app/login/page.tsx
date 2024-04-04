2a2d9c3e136f86077952b85a82673e1a86bc2f25
Touutae-lab <pantakan.totae@gmail.com>
2024-04-04 12:24:48 +0700

    Init project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,106 @@
+import Link from "next/link";
+
+const Authchentication: React.FC = () => {
+    return (
+        <div className="flex flex-col h-full justify-between">
+            <header className="absolute inset-x-0 top-0 z-50">
+                <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    <div className="flex lg:flex-1">
+                        <Link href="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Vening machine</span>
+                            <img
+                                className="h-8 w-auto"
+                                src="https://www.vectorlogo.zone/logos/redpoint/redpoint-icon.svg"
+                                alt=""
+                            />
+
+                            
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+
+        <div  className="flex min-h-full flex-1 flex-row justify-center px-6 py-12 lg:px-8">
+
+            
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://www.vectorlogo.zone/logos/redpoint/redpoint-icon.svg"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-slate-50">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-slate-50">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 dark:text-slate-50">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-red-600 hover:text-red-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-red-600 hover:text-red-500">
+              Register
+            </a>
+          </p>
+        </div>
+
+        </div>
+
+      </div>
+
+      
+    </div>
+    );
+}
+
+export default Authchentication;
